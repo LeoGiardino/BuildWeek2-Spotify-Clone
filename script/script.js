81d252e5f3c52b1d77a98d3515142fdd092face0
LeoGiardino <panto.giardino@gmail.com>
2023-12-15 09:13:42 +0100

    updata

--- script/script.js
+++ script/script.js
@@ -5,22 +5,124 @@ let idAlbum = "75621062";
 let idArtist = "412";
 console.log("object");
 
-prova();
 
-function prova () {
 
-    for(let i=90; i<110; i++){
-        fetch( `https://striveschool-api.herokuapp.com/api/deezer/artist/${i}`, {
-        method: 'GET',
+fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/366045987`, {
+    method: 'GET',
+})
+    .then(response => response.json())
+    .then(json => {
+        prova(json);
     })
-        .then(response => response.json())
-        .then(json => {
-    
-            console.log("album", json.tracklist);
-        })
-        .catch(error => console.log(i, error))
+    .catch(error => console.log(error))
+
+
+function prova(json) {
+    const titoloBC = document.querySelector(".card-title");
+    titoloBC.textContent = json.title;
+
+    const imgBC = document.querySelector(".imgBC");
+    imgBC.src = json.cover;
+
+    const text1 = document.querySelector(".card-text1");
+    let arr = [];
+
+    for (let i = 0; i < json.contributors.length; i++) {
+        let x = json.contributors[i].name;
+        arr.push(x);
     }
+
+    const contributorsText = arr.join(', ');
+
+    text1.textContent = contributorsText;
+
+    console.log(arr);
+
+    const bodyz = document.querySelector("body");
+
+    const playPause = document.querySelector("#playPause");
+
+    const muteButton = document.getElementById("muteButton");
+    const audio = new Audio(json.tracks.data[0].preview);
+
+    const volumeControl = document.getElementById("customRange1");
+
+    const playBC = document.querySelector(".playBC");
+
+    let isPlaying = false;
+
+
+    function music (){
+        if (!isPlaying) {
+            audio.play();
+            isPlaying = true;
+            playBC.textContent = "Pause";
+            playPause.innerHTML = '<i class="bi bi-pause-circle-fill"></i>';
+        } else {
+            audio.pause();
+            isPlaying = false;
+            playBC.textContent = "Play";
+            playPause.innerHTML = '<i class="bi bi-play-circle-fill"></i>';
+        }
+    }
+
+    playBC.addEventListener("click", music);
+
+
+    // Aggiunto l'evento "input" per il controllo del volume
+    volumeControl.addEventListener("input", () => {
+        // Ottieni il valore corrente del controllo del volume (è un valore tra 0 e 100)
+        const volumeValue = volumeControl.value / 100;
+
+        if(volumeValue < 0.5){
+            if(volumeValue == 0){
+                muteButton.innerHTML = '<i class="bi bi-volume-mute-fill"></i>';
+            }else{
+                muteButton.innerHTML = '<i class="bi bi-volume-down-fill"></i>';
+            }       
+        }else if(volumeValue > 0.5){
+            muteButton.innerHTML = '<i class="bi bi-volume-up-fill"></i>';
+        }
+
+        // Normalizza il valore del volume da 0 a 1
+        // L'elemento audio in JavaScript utilizza un valore di volume compreso tra 0 e 1
+        // Quindi, dividiamo il valore del controllo del volume per 100 per normalizzarlo in questo intervallo
+        // Ad esempio, se il valore del controllo del volume è 50, volumeValue sarà 0.5
+        audio.volume = volumeValue;
+
+    });
+
+
+    playPause.addEventListener("click", () => {
+
+        if (!isPlaying) {
+            audio.play();
+            isPlaying = true;
+            playPause.innerHTML = '<i class="bi bi-pause-circle-fill"></i>';
+        } else {
+            audio.pause();
+            isPlaying = false;
+            playPause.innerHTML = '<i class="bi bi-play-circle-fill"></i>';
+        }
+    });
+
+    // Aggiunto l'evento "click" per il controllo del muto
+    muteButton.addEventListener("click", () => {
+        // Inverti lo stato di muto dell'elemento audio
+        audio.muted = !audio.muted;
+
+        // Aggiorna dinamicamente l'icona in base allo stato di muto
+        if (audio.muted) {
+            // Se l'audio è muto, cambia l'icona a "volume-mute"
+            muteButton.innerHTML = '<i class="bi bi-volume-mute-fill"></i>';
+        } else {
+            // Se l'audio non è muto, torna all'icona normale
+            muteButton.innerHTML = '<i class="bi bi-volume-down-fill"></i>';
+        }
+    });
+
 }
+
 
 
 
@@ -35,20 +137,20 @@ fetch(urlArtist + idArtist, {
     .catch(error => console.log(error))
 
 
-fetch(" https://striveschool-api.herokuapp.com/api/deezer/artist/412/top?limit=10", {
+fetch(" https://striveschool-api.herokuapp.com/api/deezer/artist/412/top?limit=1", {
     method: 'GET',
 })
     .then(response => response.json())
     .then(json => {
 
-        console.log("trackList100",json);
+        console.log("trackList100", json);
     })
     .catch(error => console.log(error))
 
 
 
 
-fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=black-sabbath", {
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=viola", {
     method: 'GET',
 })
     .then(response => response.json())
